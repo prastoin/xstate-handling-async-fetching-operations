1a461468e7bec934bccbb71a8c8aa9d388f67468
prastoin <prastoin@student.42.fr>
2022-09-13 08:10:24 +0200

    chore: npm init vue@latest app + refactor using api composition

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>x-state-handling-async-fetching-operations</h1>
+  </header>
+  <main>
+    <div>
+      <button @click="decreaseCounter">-</button>
+      <span>{{ counter }}</span>
+      <button @click="increaseCounter">+</button>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const counter = ref<number>(10);
+
+    const decreaseCounter = () => {
+      counter.value--;
+    };
+
+    const increaseCounter = () => {
+      counter.value++;
+    };
+
+    return {
+      counter,
+      increaseCounter,
+      decreaseCounter,
+    };
+  },
+};
+</script>
---
+++ src/components/__tests__/App.cy.ts
@@ -0,0 +1,8 @@
+import App from '../../App.vue'
+
+describe('App', () => {
+  it('renders properly the header', () => {
+    cy.mount(App)
+    cy.get('template').should('contain', /x-state-handling-async-fetching-operations/i)
+  })
+})
