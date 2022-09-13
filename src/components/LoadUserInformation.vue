f2b93bad7d55f30c5b75ea23d5d0e27c37e4faba
prastoin <tkesray@gmail.com>
2022-09-13 16:24:56 +0200

    refactor: improve LoadUserInformation layout semantic using template tag

--- src/components/LoadUserInformation.vue
+++ src/components/LoadUserInformation.vue
@@ -1,22 +1,6 @@
-<template>
-    <div v-if="showLoadUserInformationButton">
-        <span>{{ loadUserInformationState.value }}</span>
-        <button @click="loadUserInformationButtonOnClick">Load user information</button>
-    </div>
-    <div v-else-if="isLoading">
-        <span>{{ loadUserInformationState.value }}</span>
-        <span>Loading...</span>
-    </div>
-    <div v-else>
-        <span>{{ loadUserInformationState.value }}</span>
-        <span>{{ loadUserInformationState.context.userInformation }}</span>
-    </div>
-
-</template>
-  
 <script setup lang="ts">
 import { createLoadUserInformationMachine } from '@/machines/LoadUserInformationMachine';
-import { computed } from '@vue/reactivity';
+import { computed } from 'vue';
 import { useMachine } from '@xstate/vue';
 
 const loadUserInformationMachine = createLoadUserInformationMachine();
@@ -34,4 +18,20 @@ const isLoading = computed(() => loadUserInformationState.value.value === "Loadi
 const showLoadUserInformationButton = computed(() => loadUserInformationState.value.value === "Idle" || loadUserInformationState.value.value === "Loading user information failed")
 
 </script>
-  
+
+<template>
+    <div>
+        <template v-if="showLoadUserInformationButton">
+            <span>{{ loadUserInformationState.value }}</span>
+            <button @click="loadUserInformationButtonOnClick">Load user information</button>
+        </template>
+        <template v-else-if="isLoading">
+            <span>{{ loadUserInformationState.value }}</span>
+            <span>Loading...</span>
+        </template>
+        <template v-else>
+            <span>{{ loadUserInformationState.value }}</span>
+            <span>{{ loadUserInformationState.context.userInformation }}</span>
+        </template>
+    </div>
+</template>
