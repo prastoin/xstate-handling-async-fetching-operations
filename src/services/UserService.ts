1fea87a3d5ef1b803dcd59c8b2d2500bc9a7c18d
prastoin <tkesray@gmail.com>
2022-09-16 15:07:55 +0200

    refactor: userService using redaxios and zod

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,11 +1,12 @@
-import type { UserInformation } from "@/type";
+import { SERVER_ENDPOINT, UserInformation, } from "@/type";
+import axios from "redaxios"
 
 export async function fetchUserInformation(): Promise<UserInformation> {
     console.log("fetchUserInformation")
-    return {
-        email: "",
-        id: "",
-        name: "",
-        password: ""
-    }
+    const url = `${SERVER_ENDPOINT}/user-information`
+    const response = await axios.get(url);
+    const user = UserInformation.parse(response.data)
+    console.log({ user })
+
+    return user
 }
--- src/type.ts
+++ src/type.ts
@@ -1,6 +1,11 @@
-export interface UserInformation {
-    id: string
-    name: string
-    email: string
-    password: string
-}
+import * as z from "zod"
+
+export const UserInformation = z.object({
+    id: z.string().uuid(),
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string()
+})
+export type UserInformation =  z.infer<typeof UserInformation>
+
+export const SERVER_ENDPOINT = "http:localhost:3333/"
