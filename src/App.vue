02e7472e60fadd3303755754b222627699ceccf7
prastoin <tkesray@gmail.com>
2022-09-16 15:07:21 +0200

    refactor: remove counter machine from codebase

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,11 @@
 <script lang="ts">
-import { useMachine } from '@xstate/vue';
-import { createCounterMachine } from './machines/CounterMachine';
 import LoadUserInformation from './components/LoadUserInformation.vue';
-
-const counterMachine = createCounterMachine();
 
 export default {
     setup() {
-        const { send: sendToCounterMachine, state: counterMachineState } = useMachine(counterMachine);
-        function decreaseCounterButtonOnClick() {
-            sendToCounterMachine({
-                type: "User pressed descrease button"
-            });
-        }
-        function increaseCounterButtonOnClick() {
-            sendToCounterMachine({
-                type: "User pressed increase button"
-            });
-        }
-        // why does counterMachineState.context doesn't seem to exist here but does inside the vue template ?
-        console.log(counterMachineState.value);
-        return {
-            decreaseCounterButtonOnClick,
-            increaseCounterButtonOnClick,
-            counterMachineState
-        };
+      return {
+
+      }
     },
     components: { LoadUserInformation }
 };
@@ -35,11 +16,6 @@ export default {
     <h1>x-state-handling-async-fetching-operations</h1>
   </header>
   <main>
-    <div>
-      <button @click="decreaseCounterButtonOnClick">-</button>
-      <span>{{ counterMachineState.context.counter }}</span>
-      <button @click="increaseCounterButtonOnClick">+</button>
-    </div>
     <LoadUserInformation />
   </main>
 </template>
