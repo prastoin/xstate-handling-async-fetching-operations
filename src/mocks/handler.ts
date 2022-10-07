6b5c56ef6adc62cb94ffa0f196c537836b241a1d
prastoin <prastoin@student.42.fr>
2022-10-07 09:23:01 +0200

    refactor: fetch user cart response typings

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,4 +1,4 @@
-import { FetchUserInformationResponseBody, SERVER_ENDPOINT, UserCart } from "@/type";
+import { FetchUserCartResponseBody, FetchUserInformationResponseBody, SERVER_ENDPOINT } from "@/type";
 import { rest } from "msw";
 import { faker } from "@faker-js/faker";
 
@@ -9,42 +9,44 @@ export const handlers = [
       const shouldFail = faker.datatype.boolean()
       if (shouldFail) {
         return res(
-          ctx.delay(2000),
+          ctx.delay(900),
           ctx.status(403)
         );
       }
 
+      const body = {
+        email: faker.internet.email(),
+        id: faker.datatype.uuid(),
+        name: faker.internet.userName(),
+        password: faker.internet.password(),
+      };
       return res(
-        ctx.delay(1000),
+        ctx.delay(700),
         ctx.status(200),
-        ctx.json({
-          email: faker.internet.email(),
-          id: faker.datatype.uuid(),
-          name: faker.internet.userName(),
-          password: faker.internet.password(),
-        })
+        ctx.json(body)
       );
     }
   ),
 
-  rest.get<undefined, Record<string, never>, UserCart>(
+  rest.get<undefined, Record<string, never>, FetchUserCartResponseBody>(
     `${SERVER_ENDPOINT}/user-cart`,
     (_req, res, ctx) => {
       const shouldFail = faker.datatype.boolean()
       if (shouldFail) {
         return res(
-          ctx.delay(2000),
+          ctx.delay(800),
           ctx.status(403)
         );
       }
 
+      const body: FetchUserCartResponseBody = {
+        items: Array.from({ length: faker.datatype.number({ min: 1, max: 10 }) }, () => faker.commerce.product()),
+        totalPrice: faker.datatype.number()
+      }
       return res(
-        ctx.delay(2000),
+        ctx.delay(1000),
         ctx.status(200),
-        ctx.json({
-          items: Array.from({ length: faker.datatype.number({ min: 1, max: 10 }) }, () => faker.commerce.product()),
-          totalPrice: faker.datatype.number()
-        })
+        ctx.json(body)
       );
     }
   ),
